50983c8b6156ef6869e52db1852e22d77f3fdf0d
Alexander <alex.yashkin@gmail.com>
2016-02-11 13:39:16 +0300

    Added base functionality

--- PrintScreener/MainForm.Designer.cs
+++ PrintScreener/MainForm.Designer.cs
@@ -31,19 +31,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.textBoxPath = new System.Windows.Forms.TextBox();
             this.buttonBrowse = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
             this.comboBoxFormat = new System.Windows.Forms.ComboBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelFormat = new System.Windows.Forms.Label();
+            this.numericUpDownInterval = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonStartStop = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
             this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxPath
@@ -63,32 +63,33 @@
             this.buttonBrowse.UseVisualStyleBackColor = true;
             this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
             // 
-            // button2
+            // buttonOpenFolder
             // 
-            this.button2.Location = new System.Drawing.Point(428, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Location = new System.Drawing.Point(428, 13);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFolder.TabIndex = 2;
+            this.buttonOpenFolder.Text = "Open folder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
             // 
-            // label1
+            // labelPath
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(12, 18);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(76, 13);
+            this.labelPath.TabIndex = 3;
+            this.labelPath.Text = "Output folder:";
             // 
-            // label2
+            // labelName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 44);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 44);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(56, 13);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "File name:";
             // 
             // comboBoxFormat
             // 
@@ -99,28 +100,43 @@
             this.comboBoxFormat.Size = new System.Drawing.Size(121, 21);
             this.comboBoxFormat.TabIndex = 5;
             // 
-            // textBox2
+            // textBoxName
             // 
-            this.textBox2.Location = new System.Drawing.Point(95, 41);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(246, 21);
-            this.textBox2.TabIndex = 6;
+            this.textBoxName.Location = new System.Drawing.Point(95, 41);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(246, 21);
+            this.textBoxName.TabIndex = 6;
             // 
-            // label3
+            // labelFormat
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 71);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.labelFormat.AutoSize = true;
+            this.labelFormat.Location = new System.Drawing.Point(12, 71);
+            this.labelFormat.Name = "labelFormat";
+            this.labelFormat.Size = new System.Drawing.Size(62, 13);
+            this.labelFormat.TabIndex = 7;
+            this.labelFormat.Text = "File format:";
             // 
-            // numericUpDown1
+            // numericUpDownInterval
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(95, 96);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(60, 21);
-            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDownInterval.Location = new System.Drawing.Point(95, 96);
+            this.numericUpDownInterval.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Name = "numericUpDownInterval";
+            this.numericUpDownInterval.Size = new System.Drawing.Size(60, 21);
+            this.numericUpDownInterval.TabIndex = 8;
+            this.numericUpDownInterval.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             // 
             // label4
             // 
@@ -131,35 +147,38 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "label4";
             // 
-            // button3
+            // buttonStartStop
             // 
-            this.button3.Location = new System.Drawing.Point(161, 95);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Location = new System.Drawing.Point(161, 95);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartStop.TabIndex = 10;
+            this.buttonStartStop.Text = "Start";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(347, 123);
+            this.button4.Location = new System.Drawing.Point(347, 95);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 11;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // richTextBox1
+            // richTextBoxLog
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 123);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(329, 126);
-            this.richTextBox1.TabIndex = 12;
-            this.richTextBox1.Text = "";
+            this.richTextBoxLog.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBoxLog.Location = new System.Drawing.Point(12, 123);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.ReadOnly = true;
+            this.richTextBoxLog.Size = new System.Drawing.Size(491, 126);
+            this.richTextBoxLog.TabIndex = 12;
+            this.richTextBoxLog.Text = "";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(347, 153);
+            this.button5.Location = new System.Drawing.Point(428, 95);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 13;
@@ -172,17 +191,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(515, 261);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBoxLog);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonStartStop);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numericUpDownInterval);
+            this.Controls.Add(this.labelFormat);
+            this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.comboBoxFormat);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.buttonOpenFolder);
             this.Controls.Add(this.buttonBrowse);
             this.Controls.Add(this.textBoxPath);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -193,7 +212,7 @@
             this.Text = "PrintScreener";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -203,17 +222,17 @@
 
         private System.Windows.Forms.TextBox textBoxPath;
         private System.Windows.Forms.Button buttonBrowse;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOpenFolder;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Label labelName;
         private System.Windows.Forms.ComboBox comboBoxFormat;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelFormat;
+        private System.Windows.Forms.NumericUpDown numericUpDownInterval;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonStartStop;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
         private System.Windows.Forms.Button button5;
     }
 }
--- PrintScreener/MainForm.cs
+++ PrintScreener/MainForm.cs
@@ -1,21 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace PrintScreener
 {
     public partial class MainForm : Form
     {
-        //private string format;
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        private const int WM_DRAWCLIPBOARD = 0x0308; // WM_DRAWCLIPBOARD message
+        private IntPtr clipboardViewerNext; // Our variable that will hold the value to identify the next window in the clipboard viewer chain
+
+        ImageFormat fileFormat = ImageFormat.Png;
 
         private Dictionary<string, string> formatList;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m); // Process the message 
+
+            if (m.Msg == WM_DRAWCLIPBOARD)
+            {
+                IDataObject iData = Clipboard.GetDataObject(); // Clipboard's data
+
+                //if (iData.GetDataPresent(DataFormats.Text))
+                //{
+                //    string text = (string)iData.GetData(DataFormats.Text);
+                //}
+
+                // TODO: use checkbox to enable/disable autocapture of "PrintScreen" button
+                if (iData.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap image = (Bitmap)iData.GetData(DataFormats.Bitmap);
+
+                    try
+                    {
+                        string outputFilePath = getOutputFilePath();
+
+                        image.Save(outputFilePath, fileFormat);
+                        WriteInLog(string.Format("\"{0}\" saved.", Path.GetFileName(outputFilePath)));
+                    }
+                    catch (Exception ex)
+                    {
+                        if (shotTimer != null && shotTimer.Enabled)
+                        {
+                            shotTimer.Stop();
+                            numericUpDownInterval.Enabled = true;
+                            buttonStartStop.Text = "Start";
+                            WriteInLog("Task is stopped");
+                        }
+                        WriteInLog(ex.Message);
+                    }
+                }
+            }
+        }
 
         public MainForm()
         {
@@ -24,18 +72,27 @@ namespace PrintScreener
             formatList = new Dictionary<string, string>();
             formatList.Add("jpg", "JPEG (*.jpg)");
             formatList.Add("png", "PNG (*.png)");
+            formatList.Add("gif", "GIF (*.gif)");
             formatList.Add("bmp", "BMP (*.bmp)");
         }
 
+        char[] invalidChars = Path.GetInvalidPathChars();
+
         private void MainForm_Load(object sender, EventArgs e)
         {
-            // path from settings
+            // output path from settings
             if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.Path) && Directory.Exists(Properties.Settings.Default.Path))
                 textBoxPath.Text = Properties.Settings.Default.Path;
             else
                 textBoxPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-            // format
+            // file name
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.Name) && !invalidChars.Any(Properties.Settings.Default.Name.Contains))
+                textBoxName.Text = Properties.Settings.Default.Name;
+            else
+                textBoxName.Text = "Screenshot %date% %time%";
+
+            // file format
             int index = 0, selectedIndex = 0;
             comboBoxFormat.Items.Clear();
             foreach (KeyValuePair<string, string> kvp in formatList)
@@ -46,6 +103,27 @@ namespace PrintScreener
                 index++;
             }
             comboBoxFormat.SelectedIndex = selectedIndex;
+
+            // interval
+            if (Properties.Settings.Default.Interval > 0)
+                numericUpDownInterval.Value = Properties.Settings.Default.Interval;
+
+            // clipboard
+            clipboardViewerNext = SetClipboardViewer(this.Handle);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ChangeClipboardChain(this.Handle, clipboardViewerNext); // Removes our from the chain of clipboard viewers when the form closes.
+
+            var comboBoxItemFormat = (ComboBoxItem)comboBoxFormat.SelectedItem;
+
+            // save settings
+            Properties.Settings.Default.Format = comboBoxItemFormat.Value;
+            Properties.Settings.Default.Name = textBoxName.Text;
+            Properties.Settings.Default.Path = textBoxPath.Text;
+            Properties.Settings.Default.Interval = Convert.ToInt32(Math.Round(numericUpDownInterval.Value, 0));
+            Properties.Settings.Default.Save();
         }
 
         private void buttonBrowse_Click(object sender, EventArgs e)
@@ -65,14 +143,129 @@ namespace PrintScreener
             }
         }
 
-        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        private void buttonOpenFolder_Click(object sender, EventArgs e)
         {
-            var comboBoxItemFormat = (ComboBoxItem)comboBoxFormat.SelectedItem;
+            if (!string.IsNullOrWhiteSpace(textBoxPath.Text) && Directory.Exists(textBoxPath.Text))
+                Process.Start(textBoxPath.Text);
+            else
+                MessageBox.Show("Path not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
 
-            // save settings
-            Properties.Settings.Default.Format = comboBoxItemFormat.Value;
-            Properties.Settings.Default.Path = textBoxPath.Text;
-            Properties.Settings.Default.Save();
+        /// <summary>
+        /// Get full path of image
+        /// </summary>
+        /// <returns>Image Path</returns>
+        private string getOutputFilePath()
+        {
+            string outputPath = textBoxPath.Text;
+            string fileName = textBoxName.Text;
+
+            if (string.IsNullOrWhiteSpace(outputPath) || invalidChars.Any(outputPath.Contains))
+                throw new Exception("Wrong output folder path!");
+
+            if (string.IsNullOrWhiteSpace(fileName) || invalidChars.Any(fileName.Contains))
+                throw new Exception("Wrong file name!");
+
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+
+            var comboBoxItemFormat = (ComboBoxItem)comboBoxFormat.SelectedItem;
+            string format = comboBoxItemFormat.Value;
+
+            switch (format)
+            {
+                case "png":
+                    fileFormat = ImageFormat.Png;
+                    break;
+                case "gif":
+                    fileFormat = ImageFormat.Gif;
+                    break;
+                case "bmp":
+                    fileFormat = ImageFormat.Bmp;
+                    break;
+                default:
+                    fileFormat = ImageFormat.Jpeg;
+                    break;
+            }
+
+            fileName = fileName.Replace("%date%", DateTime.Now.ToString("yyyy-MM-dd")).Replace("%time%", DateTime.Now.ToString("HH-mm-ss"));
+
+            string path = Path.Combine(textBoxPath.Text, fileName + "." + format);
+
+            int count = 2;
+            while (File.Exists(path))
+            {
+                path = Path.Combine(textBoxPath.Text, fileName + " (" + count + ")." + format);
+                count++;
+            }
+
+            return path;
+        }
+
+        private void GetSreenshot()
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            try
+            {
+                string outputFilePath = getOutputFilePath();
+
+                printscreen.Save(outputFilePath, fileFormat);
+                WriteInLog(string.Format("\"{0}\" saved.", Path.GetFileName(outputFilePath)));
+            }
+            catch (Exception ex)
+            {
+                if (shotTimer != null && shotTimer.Enabled)
+                {
+                    shotTimer.Stop();
+                    numericUpDownInterval.Enabled = true;
+                    buttonStartStop.Text = "Start";
+                    WriteInLog("Task is stopped");
+                }
+                WriteInLog(ex.Message);
+            }
+        }
+
+        private Timer shotTimer;
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (shotTimer != null && shotTimer.Enabled)
+            {
+                shotTimer.Stop();
+                numericUpDownInterval.Enabled = true;
+                (sender as Button).Text = "Start";
+                WriteInLog("Task is stopped");
+                return;
+            }
+
+            int interval = Convert.ToInt32(Math.Round(numericUpDownInterval.Value, 0));
+
+            shotTimer = new Timer(); // System.Windows.Forms.Timer
+            shotTimer.Tick += new EventHandler(shotTimer_Tick);
+            shotTimer.Interval = interval * 1000; // in miliseconds
+            shotTimer.Start();
+
+            numericUpDownInterval.Enabled = false;
+            (sender as Button).Text = "Stop";
+
+            WriteInLog(string.Format("Task is started every {0} second(s)", interval));
+
+            //var timer = new System.Threading.Timer((ev) => GetSreenshot(), null, TimeSpan.Zero, TimeSpan.FromSeconds(20));
+        }
+
+        private void shotTimer_Tick(object sender, EventArgs e)
+        {
+            GetSreenshot();
+        }
+
+        private void WriteInLog(string message)
+        {
+            richTextBoxLog.AppendText(string.Format("[{1}] {0}\n", message, DateTime.Now.ToString("G")));
+            richTextBoxLog.SelectionStart = richTextBoxLog.Text.Length; //Set the current caret position at the end
+            richTextBoxLog.ScrollToCaret(); //Now scroll it automatically
         }
     }
 }
